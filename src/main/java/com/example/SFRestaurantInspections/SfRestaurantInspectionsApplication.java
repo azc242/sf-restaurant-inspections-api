f29ffd3e975506c8d375996a8c432802191bb6ad
Alan Chen <azc242@nyu.edu>
2021-01-04 16:37:12 -0800

    initial commit

---
+++ src/main/java/com/example/SFRestaurantInspections/SfRestaurantInspectionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SFRestaurantInspections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfRestaurantInspectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfRestaurantInspectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SFRestaurantInspections/SfRestaurantInspectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SFRestaurantInspections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfRestaurantInspectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
